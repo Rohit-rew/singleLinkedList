c22720e871bd974f8b4c3f8530b0751192e7c4f3
Rohit Kumar <rohit.rew@gmail.com>
2022-12-07 20:05:09 +0530

    add and remove features created

---
+++ main.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* next;
+};
+
+class LinkedList{
+public:
+    
+    Node* head;
+    
+    int addAtTop(int val){
+        Node* NewNode = new Node();
+        NewNode->data = val;
+        
+        if(this->head == NULL){
+            head = NewNode;
+            return val;
+        }else{
+            NewNode->next = this->head;
+            head = NewNode;
+            return val;
+        }
+    }
+    
+    int addAtEnd(int val){
+        Node* NewNode = new Node;
+        NewNode->data = val;
+        Node* currentNode = head;
+        
+        while(currentNode->next != NULL){
+            currentNode = currentNode->next;
+        }
+        currentNode->next = NewNode;
+        return val;
+    }
+    
+    int addAt(int val , int index){
+        Node* NewNode = new Node;
+        NewNode->data = val;
+        
+        Node* currentNode = head;
+        int nodeNum = 0;
+        while (nodeNum != index-1 ) {
+            nodeNum++;
+            currentNode = currentNode->next;
+        }
+        
+        NewNode->next =currentNode->next;
+        currentNode->next = NewNode;
+        return val;
+    }
+    
+    int removeTop(){
+        if(size()==0){
+            return 0;
+        }else{
+            head = head->next;
+            return 0;
+        }
+    }
+    
+    void printAll(){
+        Node* currentNode = head;
+        cout << "print data starts" << endl;
+        while(currentNode->next!= NULL){
+            cout << currentNode->data << endl;;
+            currentNode = currentNode->next;
+        }
+        cout << currentNode->data << endl;
+        cout << "print data ends" << endl;
+    }
+    
+    int headNodeVal(){
+        return head->data;
+    }
+    
+    int endNodeVal(){
+        Node* currrentNode = head;
+        while(currrentNode->next != NULL){
+            currrentNode = currrentNode->next;
+        }
+        return currrentNode->data;
+    }
+    
+    int size(){
+        if(head==NULL){
+            return 0;
+        }
+        
+        Node* currentNode = head;
+        int size = 0;
+        while (currentNode->next != NULL) {
+            size++;
+            currentNode = currentNode->next;
+        }
+        return size+1;
+    }
+    
+};
+
+int main(int argc, const char * argv[]) {
+    
+    LinkedList* LinkList1 = new LinkedList;
+    LinkList1->addAtTop(12);  // adds node at top
+    LinkList1->addAtTop(22);  // adds node at top
+    LinkList1->addAtEnd(55); // adds node at tail
+    LinkList1->addAtTop(99);  // adds node at top
+    LinkList1->addAt(77,2); // adds value aat the given index
+    int lastNode = LinkList1->endNodeVal();  // returns node value at tail
+    LinkList1->removeTop();
+    int topNodeVal = LinkList1->headNodeVal(); // returns node value at head
+    LinkList1->printAll(); // prints all node values
+    
+    cout << "lastNode val : " << lastNode << endl;
+    cout << "head val : " << topNodeVal <<endl;
+    cout << "size : " << LinkList1->size() << endl;
+}
